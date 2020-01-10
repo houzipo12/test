4b8ecbddeaa42883f32dfcfed3a6370565f0ada2
houzipo12 <2848601503@qq.com>
2020-01-10 10:38:39 +0800

    添加功能，第三方github登陆，获取到code和token

---
+++ src/main/java/com/hou/my/controller/AuthroizeContorller.java
@@ -0,0 +1,33 @@
+package com.hou.my.controller;
+
+import com.hou.my.dto.AccessTokenDTO;
+import com.hou.my.dto.GithubUser;
+import com.hou.my.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+
+@Controller
+public class AuthroizeContorller {
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state) throws IOException {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("Iv1.d1013d5cda89c2cc");
+        accessTokenDTO.setClient_secret("031e18a97be393ba98c1f60079e6e2805da5e528");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+
+}
