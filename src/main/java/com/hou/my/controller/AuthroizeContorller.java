ffd638cc73908b1b6aba3825f4d5b2ed529e9e25
houzipo12 <2848601503@qq.com>
2020-01-14 10:30:26 +0800

    连接mysql数据库

--- src/main/java/com/hou/my/controller/AuthroizeContorller.java
+++ src/main/java/com/hou/my/controller/AuthroizeContorller.java
@@ -2,32 +2,62 @@ package com.hou.my.controller;
 
 import com.hou.my.dto.AccessTokenDTO;
 import com.hou.my.dto.GithubUser;
+import com.hou.my.mapper.UserMapper;
+import com.hou.my.modle.User;
 import com.hou.my.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 public class AuthroizeContorller {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.cliend.id}")
+    private String clientId;
+    @Value("${github.setClient_secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUti;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state) throws IOException {
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request) throws IOException {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("Iv1.d1013d5cda89c2cc");
-        accessTokenDTO.setClient_secret("031e18a97be393ba98c1f60079e6e2805da5e528");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUti);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+
+            //登录成功，写cookie和session
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        }else {
+            //登录失败，重新登陆
+            return"redirect:/";
+        }
     }
 
 }
---
+++ src/main/java/com/hou/my/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.hou.my.mapper;
+
+import com.hou.my.modle.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (name,accound_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}
