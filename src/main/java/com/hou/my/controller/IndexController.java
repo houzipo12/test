bd4cbf0a12b1c0180216753ae15657b88016ea5c
houzipo12 <2848601503@qq.com>
2020-01-19 22:34:41 +0800

    创建发布界面

--- src/main/java/com/hou/my/controller/IndexController.java
+++ src/main/java/com/hou/my/controller/IndexController.java
@@ -1,11 +1,31 @@
 package com.hou.my.controller;
 
+import com.hou.my.mapper.UserMapper;
+import com.hou.my.modle.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/")
-    public String index(){ return "index";}
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies){
+            if (cookie.getName().equals("token"));
+            String token = cookie.getValue();
+            User user = userMapper.findBYToken(token);
+            if (user !=null){
+                request.getSession().setAttribute("user",user);
+            }
+            break;
+        }
+        return "index";}
 
 }
--- src/main/java/com/hou/my/mapper/UserMapper.java
+++ src/main/java/com/hou/my/mapper/UserMapper.java
@@ -3,9 +3,13 @@ package com.hou.my.mapper;
 import com.hou.my.modle.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user (name,accound_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findBYToken(String token);
 }
